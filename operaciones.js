fc12dfad0d910c1fd81925b96778f12c65912c40
Adrián Torres <binarytreecode@gmail.com>
2022-02-10 14:15:52 -0500

    comienza trabajo con json

--- operaciones.js
+++ operaciones.js
@@ -1,7 +1,7 @@
 var text = "I2O5";
-i = 1;
-oxi_id = text.indexOf("O");
-oxi = text.charAt(oxi_id+i);
+var i = 1;
+var oxi_id = text.indexOf("O");
+var oxi = text.charAt(oxi_id+i);
 
 function recursiva (){
     if (isNaN(oxi)) {
@@ -13,3 +13,40 @@ function recursiva (){
         oxi = oxi;
     }
 }
+recursiva ();
+function oxidoN(elemento) {
+    switch (elemento) {
+        case "": elemento = "mono";
+        case "2": elemento = "di";
+        case "3": elemento = "tri";
+        case "4": elemento = "tetra";
+        case "5": elemento = "penta";
+        case "6": elemento = "hexa";
+        case "7": elemento = "hepta";
+        case "8": elemento = "octa";
+        case "9": elemento = "nona";
+        case "10": elemento = "deca";
+        default: elemento = "";
+    };
+}
+oxidoN(oxi)
+
+let i = 0;
+let largo = text.length;
+var oxi_no;
+while (largo > i) {
+    o = text.charAt(i);
+    if (isNaN(o)) {
+        console.log(o);
+        oxi_no = "";
+    }
+    else {
+        console.log(o);
+        oxi_no = o;
+    }
+    i++;
+}
+oxidoN(oxi_no)
+
+
+sistematica = `${oxi}oxido de ${oxi_no}${noMetal}`;
