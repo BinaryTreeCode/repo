eff5481fea1d45f425fb38a9a28b5f7283897c59
Adrián Torres <binarytreecode@gmail.com>
2022-02-14 12:45:58 -0500

    Finalización del proyecto por parte del html y js

--- operaciones.js
+++ operaciones.js
@@ -1,8 +1,41 @@
-var text = "I2O5";
-var oxi_id = text.indexOf("O");
-var oxi = text.charAt(oxi_id + i);
+var formula_ID = document.getElementById("formula");
+var tradicional_text = document.getElementById("tradicional_input");
 
-var i = 1;
+const formula_text = document.getElementById("Fórmula_text");
+const sistematica_ID = document.getElementById("Sistemática");
+const stock_ID = document.getElementById("Stock");
+const tradicional_ID = document.getElementById("Tradicional");
+
+var text = "";
+var oxiR;
+var ONM;
+//  ONM = oxiddción_no_metal
+var noMetal_beta, noMetal_id;
+var noMetal = "";
+var sistematica, stock, tradicional;
+function star() {
+    text = formula_ID.value;
+    oxi_id = text.indexOf("O");
+    i = 1;
+    oxi = text.charAt(oxi_id + i);
+    recursiva();
+    i = 0;
+    element()    
+    ONM = text.charAt(noMetal_id + 1);
+    if (isNaN(ONM)) {
+        ONM = "";
+    }
+    else {
+        ONM = ONM;
+    }
+
+    oxi = oxidoN(oxi);
+    ONM = ONMN(ONM); //  ONMN = oxiddción_no_metal_numero
+    oxiR = oxidoNR(oxi);
+    nameMetal();
+    write();
+};
+
 function recursiva() {
     if (isNaN(oxi)) {
         i++;
@@ -10,79 +43,140 @@ function recursiva() {
         recursiva();
     }
     else {
-        oxi = oxi;
+        return oxi;
     }
 }
-recursiva();
+
+function VM(palabra, n) {
+    return palabra.charAt(n) === palabra.charAt(n).toUpperCase()
+}
+function element() {
+    while (text.length > i) {
+        o = text.charAt(i);
+        m = text.charAt(i + 1);
+
+        if (VM(o, 0) && o != "O" && isNaN(o)) {
+            noMetal_beta = o;
+            noMetal_id = i;
+            console.log(noMetal_beta)
+            if (VM(m, 0) == false && m != "(" && m != ")" && m != "" && m != " ") {
+                noMetal_beta = o + m;
+                noMetal_id = i + 1;
+            }
+            break;
+        }
+        else {
+            noMetal_beta = "";
+        }
+        i++;
+    }
+}
+
 function oxidoN(elemento) {
     switch (elemento) {
         case "": elemento = "mono";
+            break;
         case "2": elemento = "di";
+            break;
         case "3": elemento = "tri";
+            break;
         case "4": elemento = "tetra";
+            break;
         case "5": elemento = "penta";
+            break;
         case "6": elemento = "hexa";
+            break;
         case "7": elemento = "hepta";
+            break;
         case "8": elemento = "octa";
+            break;
         case "9": elemento = "nona";
+            break;
         case "10": elemento = "deca";
+            break;
         default: elemento = "";
+            break;
     };
+    return elemento;
 }
-oxidoN(oxi)
-
-i = 0;
-let largo = text.length;
-var oxi_no;
-while (largo > i) {
-    o = text.charAt(i);
-    if (isNaN(o)) {
-        console.log(o);
-        oxi_no = "";
-    }
-    else {
-        console.log(o);
-        oxi_no = o;
-    }
-    i++;
+function oxidoNR(elemento) {
+    switch (elemento) {
+        case "2": elemento = "II";
+            break;
+        case "3": elemento = "III";
+            break;
+        case "4": elemento = "IV";
+            break;
+        case "5": elemento = "V";
+            break;
+        case "6": elemento = "VI";
+            break;
+        case "7": elemento = "VII";
+            break;
+        case "8": elemento = "VIII"
+            break;
+        case "9": elemento = "IX"
+            break;
+        case "10": elemento = "X"
+            break;
+        default: elemento = "I";
+            break;
+    };
+    return elemento;
 }
-oxidoN(oxi_no)
-
-
-// sistematica = `${oxi}oxido de ${oxi_no}${noMetal}`;
-
-var text
-function write(element_Name, element_Id) {
-    text =
-        `"${element_Id}": {
-        "nombre" : "${element_Name}",
-    },`
-    return text;
+function ONMN(elemento) {
+    switch (elemento) {
+        case "": elemento = "";
+            break;
+        case "2": elemento = "di";
+            break;
+        case "3": elemento = "tri";
+            break;
+        case "4": elemento = "tetra";
+            break;
+        case "5": elemento = "penta";
+            break;
+        case "6": elemento = "hexa";
+            break;
+        case "7": elemento = "hepta";
+            break;
+        case "8": elemento = "octa";
+            break;
+        case "9": elemento = "nona";
+            break;
+        case "10": elemento = "deca";
+            break;
+        default: elemento = "";
+            break;
+    };
+    return elemento;
 }
 
-async function datos(n) {
-    const response = await fetch("http://127.0.0.1:5500/Tabla%20periodica/tablaPeriodica.json");
+async function datos(simbolo) {
+    const response = await fetch("http://127.0.0.1:5500/Tabla%20periodica/JSON/pruebas.json");
     const json = await response.json();
-    console.log(json[n].nombre);
-    console.log(json[n].simbolo);
-
-    promise_nombre = json[n].nombre;
-    promise_id = json[n].simbolo;
-
-    promises = [promise_nombre, promise_id];
-    return promises;
+    promise = eval(simbolo);
+    return promise;
 };
 
-i = 0;
-var json = [];
-
-async function ciclo() {
-    while (117 > i) {
-        var valores = await datos(i);
-        write(valores[0], valores[1]);
-        json.push(text);
-        i++;
-    }
+async function nameMetal() {
+    simbolo_beta = `json.${noMetal_beta}.nombre`;
+    noMetal = await datos(simbolo_beta);
+    console.log(noMetal);
+    noMetal = noMetal.toLowerCase();
     
-}
+    sistematica = `${oxi}oxido de ${ONM}${noMetal}`;
+    stock = `oxido de ${noMetal} (${oxiR})`;
+    console.log(sistematica);
+    console.log(stock);
 
+    write();
+};
+
+
+function write() {
+    formula_text.innerHTML = text;
+    sistematica_ID.innerHTML = sistematica;
+    stock_ID.innerHTML = stock;
+    tradicional_ID.innerHTML = tradicional_text.value;
+}
